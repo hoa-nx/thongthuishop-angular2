0da22c09726d47136b1d6873b01e9f7088f7a547
thieu_nx <xuanhoa97@gmail.com>
2017-05-14 15:31:21 +0700

    tao routes cho ung dung angular2

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
+import { mainRoutes } from './main.routes';
+import { RouterModule, Routes } from '@angular/router';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(mainRoutes)
+
   ],
   declarations: [MainComponent]
 })
