c474fc64c62242faacab44dc3b5b0545ebfe0979
thieu_nx <xuanhoa97@gmail.com>
2017-05-14 21:19:17 +0700

    Lesson 13 : Apply ui to main page

--- src/app/main/main.routes.ts
+++ src/app/main/main.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main.component';
 
 export const mainRoutes: Routes = [
@@ -7,9 +7,10 @@ export const mainRoutes: Routes = [
         path: '', component: MainComponent, children: [
             //localhost:4200/main
             { path: '', redirectTo: 'home', pathMatch: 'full' },
+            //localhost:4200/main/home
+            { path: 'home', loadChildren: './home/home.module#HomeModule' },
             //localhost:4200/main/user
             { path: 'user', loadChildren: './user/user.module#UserModule' }
-
         ]
     }
 
