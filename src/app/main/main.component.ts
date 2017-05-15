0a019feca5eeb7074f2355ab3f12a24d23e3bd48
thieu_nx <xuanhoa97@gmail.com>
2017-05-16 00:03:28 +0700

    Lesson 17: Display info on ui

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { SystemConstants } from '../core/common/system.constants';
 import { UtilityService } from '../core/services/utility.service';
 import { UrlConstants } from '../core/common/url.constants';
+import { AuthenService } from '../core/services/authen.service';
+import { LoggedInUser } from '../core/domain/loggedin.user';
 
 @Component({
   selector: 'app-main',
@@ -10,9 +12,12 @@ import { UrlConstants } from '../core/common/url.constants';
 })
 export class MainComponent implements OnInit {
 
-  constructor(private utilityService: UtilityService) { }
+  constructor(private utilityService: UtilityService, private authenService: AuthenService) { }
+  public user: LoggedInUser;
 
   ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem(SystemConstants.CURRENT_USER));
+
   }
 
   logout() {
