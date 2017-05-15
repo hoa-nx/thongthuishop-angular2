617bcd20bdab8e457ed7606589ba014f8d6e622d
thieu_nx <xuanhoa97@gmail.com>
2017-05-16 00:36:27 +0700

    Lesson 18: protect application with guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const appRoutes: Routes = [
     //localhost:4200
@@ -6,6 +7,5 @@ export const appRoutes: Routes = [
     //localhost:4200/login
     { path: 'login', loadChildren: './login/login.module#LoginModule' },
     //localhost:4200/main
-    { path: 'main', loadChildren: './main/main.module#MainModule' }
-
+    { path: 'main', loadChildren: './main/main.module#MainModule', canActivate: [AuthGuard] }
 ];
